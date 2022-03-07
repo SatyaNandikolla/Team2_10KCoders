2561ae41955dec76860ff66530842396c3d662cb
SatyaNandikolla <satyanandikolla@gmail.com>
2022-03-07 19:16:37 +0530

    changes

--- REACTJS/app2/src/App.js
+++ REACTJS/app2/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { Murali, Krishna, Sam } from './Murali';
-import Some from './Narendhra';
+import { Kiran } from './Narendhra';
+import Abc from './Narendhra';
+import { Harish } from './Harish';
 
 
 function App() {
   return (
     <div className="App">
       <h2>Hello From App</h2>
-      <Murali />
+      {/* <Murali />
       <Krishna />
       <Sam />
-      {console.log(Some)} 
+      <Kiran />
+      <Abc/> */}
+      <Harish/>
     </div>
   );
 }
--- REACTJS/app2/src/Narendhra.jsx
+++ REACTJS/app2/src/Narendhra.jsx
@@ -1,19 +1,27 @@
 //Class Component
 import React, { Component } from "react";
 export class Narendhra extends Component {
-    render() {
-        return <h2>I Am From Narendhra Class Component</h2>
-    }
+//javascript way of displaying the value from javascript to html
+//var message = "Hello From Narendhra Class Component"
+//document.querySelector("h3").innerHTML = message
+
+constructor(){
+    this.state={}
 }
- 
-export class Kiran extends Component{
-    render(){
-        return<h2>I Am From Kiran Class Component</h2>
+
+render() {
+    return <h2></h2>
+}
+}
+
+export class Kiran extends Component {
+    render() {
+        return <h2>I Am From Kiran Class Component</h2>
     }
 }
 
-export default class Divya extends Component{
-    render(){
-        return<h2>Hello I Am From Divya Class Component</h2>
+export default class Divya extends Component {
+    render() {
+        return <h2>Hello I Am From Divya Class Component</h2>
     }
 }
