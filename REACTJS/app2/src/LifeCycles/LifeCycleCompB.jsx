dc9144b6578c49ec8ff67f039b07299fad4442ae
SatyaNandikolla <satyanandikolla@gmail.com>
2022-03-12 12:55:44 +0530

    changes

--- REACTJS/app2/src/LifeCycles/LifeCycleCompB.jsx
+++ REACTJS/app2/src/LifeCycles/LifeCycleCompB.jsx
@@ -1,4 +1,7 @@
+//umMounting
 import React, { Component } from "react";
+import ChildComp from "./ChildComp";
+import ConditionComponent from "./ConditionComponent";
 export default class LifeCycleCompB extends Component{
     //rcon-react constructor
     // constructor(props){
@@ -10,15 +13,24 @@ export default class LifeCycleCompB extends Component{
     constructor(props){
         super(props)
         this.state={
-
+            count:0
         }
-        console.log("Hello From LifeCycleCompB Constructor")
+     
+    }
+    increase=()=>{
+        this.setState({count:this.state.count+1})
     }
     render(){
-        console.log("Hello From LifeCycleCompB Render")
+
         return (
             <div>
-                <h2>Hello From LifeCycleCompB -{this.props.count}</h2>
+                {/* <h2>Hello From LifeCycleCompB -{this.props.count}</h2> */}
+                <button onClick={this.increase}>Increase Count</button>
+                <ChildComp count={this.state.count}/>
+                <hr/>
+                {/* condition  */}
+                {this.state.count < 3 && <ConditionComponent/>}
+
             </div>
         )
     }
