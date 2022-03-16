79c50d720850d30dca200cd60812ab58d1d7e4b5
SatyaNandikolla <satyanandikolla@gmail.com>
2022-03-16 15:13:06 +0530

    changes

---
+++ REACTJS/app2/src/Redux/Actions/index.js
@@ -0,0 +1,16 @@
+function getAllStudents(){
+    return{
+        type:"GET_ALL_STUDENTS"
+    }
+}
+function getAllProducts(){
+    return{
+        type:"GET_ALL_PRODUCTS"
+    }
+}
+function getAllItems(){
+    return{
+        type:"GET_ALL_ITEMS"
+    }
+}
+//Always Actions Will Return An Object(it is a rule)
--- REACTJS/app2/src/index.js
+++ REACTJS/app2/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from './Redux/Reducer';
+//step:3
+const store = createStore(rootReducer);
+console.log(store)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
