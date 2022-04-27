5bc79413f6318c387098f475e0af77c8a1a0da8b
SatyaNandikolla <satyanandikolla@gmail.com>
2022-04-27 10:04:39 +0530

    changes

--- resume_project/src/About.jsx
+++
@@ -1,9 +0,0 @@
-//rafc
-import React from "react"
-export const About=()=>{
-    return(
-        <div>
-            <h2>Hello I Am From About Component</h2>
-        </div>
-    )
-}
--- resume_project/src/App.js
+++ resume_project/src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import { About } from "./About";
+import { About } from "./Components/About";
 import { Home } from "./Home";
 import { Users } from "./Users";
+import './css/style.default.css';
+import './css/custom.css'
+import { Header } from "./Components/Header";
+import { Slider } from "./Components/Slider";
+import { Expertise } from "./Components/Expertise";
+import { Education } from "./Components/Education";
+import { Experience } from "./Components/Experience";
+import { Contact } from "./Components/Contact";
+import { Footer } from "./Components/Footer";
+
 
 function App() {
   return (
     <div>
-      <h2>Hello From Routing App</h2>
+      
       <BrowserRouter>
-      <Link to="/">Home</Link> ||
-        <Link to="about">About</Link>
+      <Header/>
+      <Slider/>
+      
         <Routes>
-          <Route path="about" element={<About />}/>
-            <Route path="users" element={<Users />}/>
-              <Route path="" element={<Home />}/>
+          <Route path="" element={<About/>}/>
+          <Route path="about" element={<About/>}/>
+            <Route path="expertise" element={<Expertise/>}/>
+            <Route path="education" element={<Education/>}/>
+            <Route path="experience" element={<Experience/>}/>
+            <Route path="contact" element={<Contact/>}/>
               </Routes>
+              <Footer/>
               </BrowserRouter>
               </div>
           );
