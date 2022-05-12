d979b30fa2c213bb99c1d099455276c555562b88
SatyaNandikolla <satyanandikolla@gmail.com>
2022-05-12 15:08:01 +0530

    changes

--- REACTJS/practice_app1/src/App.js
+++ REACTJS/practice_app1/src/App.js
@@ -2,6 +2,11 @@
 // import React from "react";
 // import { ComponentC } from "./CONTEXT(what i understand)/ComponentC";
 
+import Harish from "./Harish";
+import Murali from "./Murali";
+import { Narendhra } from "./Narendhra";
+import {Sandhya, Sonia} from "./Sandhya";
+
 // export const UserContext=React.createContext()
 
 // export default function App(){
@@ -15,19 +20,32 @@
 // }
 
 
-import React from "react";
-import { ComponentContainer } from "./Pure Components/ComponentContainer";
-import Students from "./Students";
-import Users from "./Users";
+// import React from "react";
+// import { ComponentContainer } from "./Pure Components/ComponentContainer";
+// import Students from "./Students";
+// import Users from "./Users";
 
 
 
+// export default function App(){
+//    return(
+//     <div>
+//          {/* <ComponentContainer/> */}
+//          {/* <Students/> */}
+//          <Users/>
+//     </div>
+//   )
+// }
 export default function App(){
-   return(
+  return(
     <div>
-         {/* <ComponentContainer/> */}
-         {/* <Students/> */}
-         <Users/>
+    {/* <h2>Hello I Am From App Component</h2>
+    <Murali/>
+    <Sandhya/>
+    <Sonia/> */}
+    {/* <Narendhra/> */}
+    {/* <Harish/> */}
+    <Studen
     </div>
   )
 }
